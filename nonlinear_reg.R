f3e196603c57c16d68547133ce2d53772e118edb
Matheus dos Santos Rabetti <matheus.rabetti@gmail.com>
2016-03-31 14:01:32 -0300

    Create nonlinear_reg.R

---
+++ nonlinear_reg.R
@@ -0,0 +1,242 @@
+# Autor: Matheus Rabetti
+# Ultima Att: 30/03/2016
+# Versao: 1.0
+
+
+# PRIMEIROS PASSOS --------------------------------------------------------
+
+rm(list = ls())
+gc()
+
+setwd("C:/Users/b2562360/Documents/Work/")
+
+agric <- read.csv("eustaquio.csv", sep = ";", header = T, dec = ",", 
+                  colClasses = rep("numeric", 4))
+
+
+# COMANDOS ----------------------------------------------------------------
+
+library(proto)
+as.lm <- function(object, ...) UseMethod("as.lm")
+
+as.lm.nls <- function(object, ...) {
+  if (!inherits(object, "nls")) {
+    w <- paste("expected object of class nls but got object of class:", 
+               paste(class(object), collapse = " "))
+    warning(w)
+  }
+  
+  gradient <- object$m$gradient()
+  if (is.null(colnames(gradient))) {
+    colnames(gradient) <- names(object$m$getPars())
+  }
+  
+  response.name <- if (length(formula(object)) == 2) "0" else 
+    as.character(formula(object)[[2]])
+  
+  lhs <- object$m$lhs()
+  L <- data.frame(lhs, gradient)
+  names(L)[1] <- response.name
+  
+  fo <- sprintf("%s ~ %s - 1", response.name, 
+                paste(colnames(gradient), collapse = "+"))
+  fo <- as.formula(fo, env = as.proto.list(L))
+  
+  do.call("lm", list(fo, offset = substitute(fitted(object))))
+  
+}
+
+
+
+# SIMULACAO ---------------------------------------------------------------
+
+
+# Gráfico de simulacao 
+# Modelo exponencial de fonte central
+x = seq(0, 20, by = .05)
+n = length(x)
+
+a = 0.5
+plot(curve(100 * (1 - exp(-a*x)), 1, NULL, n = n), 
+     type = "l", xlim = c(0, 20), ylim = c(0, 100),
+     xlab = "Período", ylab = "Y")
+
+curve(100 * (1 - exp(-a*x)), 1, NULL, 
+      n = n, add=T)
+text(13.6, 97, expression(alpha[1] == 0.5), cex = .8)
+
+a = 0.2
+curve(100 * (1 - exp(-a*x)), 1, NULL, 
+      n = n, add=T, lty = 2)
+text(17, 93, expression(alpha[2] == 0.2), cex = .8)
+
+a = 0.1
+curve(100 * (1 - exp(-a*x)), 1, NULL, 
+      n = n, add=T, lty = 3)
+text(17.5, 79, expression(alpha[3] == 0.1), cex = .8)
+
+a = 0.05
+curve(100 * (1 - exp(-a*x)), 1, NULL, 
+      n = n, add=T, lty = 6)
+text(18.2, 55, expression(alpha[4] == 0.05), cex = .8)
+
+legend(-0.5, 100, c(1:4), title = "Regiões",
+       lty = c(1, 2, 3, 6))
+
+
+# Modelo logístico de contágio
+# k posiciona a curva =
+# x é o tempo
+# b é a taxa de adoção
+# N é teto potencial de inovadores no mercado, tamanho da populacao N = 100
+b = 1
+plot(1,type = "l", xlim = c(0, 20), ylim = c(0, 100),
+     xlab = "Período", ylab = "Y")
+
+curve(100 * (1 + exp(-(b*x)+1.5))^(-1), 0.5, NULL, n = n, add=T)
+text(12, 97, expression(beta[1] == 1), cex = .8)
+
+b = 0.4
+curve(100 * (1 + exp(-(b*x)+3))^(-1), 0.5, NULL, n = n, add=T, lty = 2)
+text(17, 93, expression(beta[2] == 0.4), cex = .8)
+
+b = 0.25
+curve(100 * (1 + exp(-(b*x)+3.5))^(-1), 0.5, NULL, n = n, add=T, lty = 3)
+text(18, 65, expression(beta[3] == 0.25), cex = .8)
+
+b = 0.1
+curve(100 * (1 + exp(-(b*x)+3.5))^(-1), 0.5, NULL, n = n, add=T, lty = 6)
+text(17, 10, expression(beta[4] == 0.1), cex = .8)
+
+legend(-0.5, 100, c(1:4), title = "Regiões",
+       lty = c(1, 2, 3, 6))
+
+
+# LINEAR ------------------------------------------------------------------
+
+
+# Regressao linear exponencial - SOJA
+t <- 1:length(na.omit(agric$Soja))
+linear_exp <- lm(log(1/(1-Soja)) ~ t + 0, data = agric)
+summary(linear_exp)
+coef(linear_exp)
+
+
+
+resultsC <- data.frame(1 - exp(-1 * predict(linear_exp, 
+                                            interval = "confidence")))
+resultsP <- data.frame(1 - exp(-1 * predict(linear_exp, 
+                                            interval = "prediction")))
+
+# Formula
+# y = 1 - exp(coef(linear_exp)*t)
+
+# Soja
+# Plotando regressao linear exponencial
+with(agric, plot(Soja~Ano, pch = 16))
+lines(agric$Ano, resultsC$fit,lwd=2,
+      col = "red", xlab = "Ano", ylab = "Soja")
+
+lines(agric$Ano, resultsC$lwr,lwd=1,
+      col = "blue", xlab = "Ano", ylab = "Soja")
+lines(agric$Ano, resultsC$upr,lwd=1,
+      col = "blue", xlab = "Ano", ylab = "Soja")
+
+lines(agric$Ano, resultsP$lwr,lwd=1,
+      col = "green", xlab = "Ano", ylab = "Soja")
+lines(agric$Ano, resultsP$upr,lwd=1,
+      col = "green", xlab = "Ano", ylab = "Soja")
+
+
+# Regressao linear logistica
+linear_log <- lm(I(log(Soja/(1-Soja))) ~ t , data = agric)
+summary(linear_log)
+coef(linear_log)
+
+
+resultsC <- data.frame(1/ (1 + exp(-1 * predict(linear_log, 
+                                                interval = "confidence"))))
+resultsP <- data.frame(1/ (1 + exp(-1 * predict(linear_log, 
+                                                interval = "prediction"))))
+
+# Formula
+# y = 1 / (1 + exp( (-1) * (Beta * t + k) ) )
+
+# Soja
+# Plotando regressao linear exponencial
+with(agric, plot(Soja~Ano, pch = 16))
+lines(agric$Ano, resultsC$fit, lwd=2,
+      col = "red", xlab = "Ano", ylab = "Soja")
+
+lines(agric$Ano, resultsC$lwr,lwd=1,
+      col = "blue", xlab = "Ano", ylab = "Soja")
+lines(agric$Ano, resultsC$upr,lwd=1,
+      col = "blue", xlab = "Ano", ylab = "Soja")
+
+lines(agric$Ano, resultsP$lwr,lwd=1,
+      col = "green", xlab = "Ano", ylab = "Soja")
+lines(agric$Ano, resultsP$upr,lwd=1,
+      col = "green", xlab = "Ano", ylab = "Soja")
+
+# NAO LINEAR --------------------------------------------------------------
+# EXP
+f <- function(x, a) {1 - exp(-a*x)}
+
+x <- t
+# fit a nonlinear model
+fm <- nls(Soja ~ f(x, a), data = agric, 
+          start = c(a = as.numeric(coef(linear_exp))))
+
+coef(fm)
+summary(as.lm.nls(fm))
+summary(fm)
+
+resultsC <- data.frame(predict(as.lm.nls(fm), interval = "confidence"))
+resultsP <- data.frame(predict(as.lm.nls(fm), interval = "prediction"))
+
+
+with(agric, plot((100*Soja)~Ano, type="n", 
+                 ylim = c(-20, 100), yaxt = "n",
+                 ylab = "Adoção (%)"))
+axis(2, at = seq(0, 100, by = 20), las=2)
+title(main = "Soja")
+# add fill
+polygon(c(agric$Ano, rev(agric$Ano)),
+        c(100*resultsP$lwr, 100*rev(resultsP$upr)),
+        col = "#D8EDFF", border = FALSE)
+polygon(c(agric$Ano, rev(agric$Ano)),
+        c(100*resultsC$lwr, 100*rev(resultsC$upr)),
+        col = "#A1C7D6", border = FALSE)
+
+with(agric, points((100*Soja)~Ano, col = "blue"))
+lines(agric$Ano, 100*predict(fm))
+
+
+
+# LOG
+f <- function(x, b, k) {(1 + exp(-b*x - k) )^(-1)}
+fm <- nls(Soja ~ f(x, b, k), data = agric, 
+          start = list(b = as.numeric(coef(linear_log)[2]),
+                       k = as.numeric(coef(linear_log)[1])))
+
+summary(as.lm.nls(fm))
+
+resultsC <- data.frame(predict(as.lm.nls(fm), interval = "confidence"))
+resultsP <- data.frame(predict(as.lm.nls(fm), interval = "prediction"))
+
+
+with(agric, plot((100*Soja)~Ano, type="n", 
+                 ylim = c(-20, 100), yaxt = "n",
+                 ylab = "Adoção (%)"))
+axis(2, at = seq(0, 100, by = 20), las=2)
+title(main = "Soja")
+# add fill
+polygon(c(agric$Ano, rev(agric$Ano)),
+        c(100*resultsP$lwr, 100*rev(resultsP$upr)),
+        col = "#D8EDFF", border = FALSE)
+polygon(c(agric$Ano, rev(agric$Ano)),
+        c(100*resultsC$lwr, 100*rev(resultsC$upr)),
+        col = "#A1C7D6", border = FALSE)
+
+with(agric, points((100*Soja)~Ano, col = "blue"))
+lines(agric$Ano, 100*predict(fm))
